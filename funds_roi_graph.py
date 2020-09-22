816a45f6443bcc8db3a875a9a44aedb7b943f414
Romain Couperier <rcouperier@gmail.com>
2020-09-22 11:40:54 +0200

    Changes to the Malta data

--- funds_roi_graph.py
+++ funds_roi_graph.py
@@ -4,6 +4,8 @@ from datetime import datetime
 import yfinance as yf
 import numpy as np
 from pycoingecko import CoinGeckoAPI
+import datetime
+from matplotlib import pyplot as plt 
 
 def get_fund_ROI(address, file_name):
     sent = requests.get('https://api.bloxy.info/widget/address_value_daily?address=' + address + '&currency=ETH&key=ACCunOMWYpmCp&format=table&price_currency=USD').json()
@@ -36,7 +38,7 @@ def get_fund_ROI(address, file_name):
 
     data = cg.get_coin_market_chart_by_id('bitcoin', 'eth', 500)
 
-    dates = [datetime.fromtimestamp(data['prices'][i][0] / 1000).strftime('%Y-%m-%d') for i in range(len(data['prices']))]
+    dates = [datetime.datetime.fromtimestamp(data['prices'][i][0] / 1000).strftime('%Y-%m-%d') for i in range(len(data['prices']))]
     price  = [data['prices'][i][1] for i in range(len(data['prices']))]
 
     df_data = {'BTC-ETH': price, 'Date' : dates} 
@@ -57,64 +59,67 @@ def get_fund_ROI(address, file_name):
 def main():
     get_fund_ROI('0xCB60D600160D005845Ec999f64266D5608fd8943', 'Fnd.csv')
     get_fund_ROI('0x9C49c053a8b9106024793516EE3c5562875A5C9a', 'Anastasia.csv')
-    get_fund_GAV_ROI('0x0d9596Afc608B3322A17118A573750045F52C0B8', 'Malta.csv')
+    Malta('Malta.csv')
 
-    now = datetime.now()
+    now = datetime.datetime.now()
     dt_string = now.strftime("%d/%m/%Y %H:%M:%S")
     f = open("run_log.txt","a")
     f.write("Ran at " + dt_string + "\n")
     f.close() 
 
-def get_fund_GAV_ROI(address, file_name):    
-    fundToken = '0x36c4abec6166b5009b0abf493ef4500d76eb02d4'
 
-    sent = requests.get('https://api.bloxy.info/money_flow/transfers?address=0x0a27a978dfe58659d724ae96d9f25b7ed6b4fd97&chain=eth&token=0xc02aaa39b223fe8d0a0e5c4f27ead9083c756cc2&direction=all&key=ACCunOMWYpmCp&format=table').json()
-    tokenDist = pd.DataFrame(sent)
-    investmentTx = tokenDist[tokenDist[3] == '0x0d9596afc608b3322a17118a573750045f52c0b8'][[1, 4, 6]]
+def Malta(file_name):
 
-    investmentTx.loc[:, 1] = pd.to_datetime(investmentTx[1]).dt.strftime('%Y-%m-%d')
-    investmentTx.rename(columns={1:'Date', 4:'side', 6:'amount'}, inplace=True)
+    # Get the NAV, and sharePrice of the fund on each priceUpdate
+    jsonData = '{"query": "{fundCalculationsHistories(where: {fund: \\"0x26491fc7da30b35d818de45982fb1de4f65ed8f5\\"}) { nav, timestamp, totalSupply, source}}"}'
+    data = requests.post('https://api.thegraph.com/subgraphs/name/melonproject/melon', data = jsonData).json()['data']
 
-    rng = pd.date_range(investmentTx.iloc[-1]['Date'], datetime.now())
+    roi = []
+    nav = []
+    timestamp = []
 
-    investmentTx.loc[investmentTx['side'] == 'to', 'amount'] = investmentTx['amount'] * -1
-    investmentTx['Date'] = pd.to_datetime(investmentTx['Date'])
+    for i in range(len(data['fundCalculationsHistories'])):
+        if (data['fundCalculationsHistories'][i]['source'] == 'priceUpdate'):
+            roi.append(int(data['fundCalculationsHistories'][i]['nav']) / int(data['fundCalculationsHistories'][i]['totalSupply']))
+            nav.append(int(data['fundCalculationsHistories'][i]['nav']) / 1e18)
+            timestamp.append((int(data['fundCalculationsHistories'][i]['timestamp'])))
 
-    a = investmentTx['amount'].groupby(investmentTx['Date'].dt.to_period('D')).sum().to_frame()
-    a = a.set_index(a.index.to_timestamp())
-    a = a.reindex(rng, fill_value=0)
-
-    a.amount = (a['amount'].cumsum())
-
-    sent = requests.get('https://api.bloxy.info/widget/address_value_daily?address=' + address + '&currency=ETH&key=ACCunOMWYpmCp&format=table&price_currency=ETH').json()
-    df2 = pd.DataFrame(sent)
-    df2.rename(columns={0:'Date', 4: 'GAV'}, inplace=True)
-    df2 = df2.set_index('Date')
-
-    data = pd.concat([a, df2], axis=1, sort=False)
-    data['ROI-ETH'] = (data['GAV'] / data['amount'] - 1) * 100
-
-    sent = requests.get('https://api.bloxy.info/widget/address_value_daily?address=' + address + '&currency=ETH&key=ACCunOMWYpmCp&format=table&price_currency=USD').json()
-    df1 = pd.DataFrame(sent)
-    df1.rename(columns={0:'Date', 11: 'ROI'}, inplace=True)
-    df1['ROI-USD'] = df1.ROI.cumsum()
-    df1 = df1.set_index('Date')
-    df1.index = pd.to_datetime(df1.index)
+    df1 = pd.DataFrame({'NAV': nav, 'ROI-ETH': roi}, index=timestamp)
 
 
-    df = pd.concat([df1, data], axis=1, sort=False)[['ROI-USD', 'ROI-ETH']]
-    # df = df.set_index('Date')
-    # df.index = pd.to_datetime(df.index)
+    # Get the price of USDC on each price update
+    jsonData = '{"query": "{assetPriceHistories(where: {asset: \\"0xa0b86991c6218b36c1d19d4a2e9eb0ce3606eb48\\", , timestamp_gt: \\"' + str(timestamp[0] - 1) + '\\"}) {price, timestamp}}"}'
+    data = requests.post('https://api.thegraph.com/subgraphs/name/melonproject/melon', data = jsonData).json()['data']
 
-    # print(df)
-    
+    price = []
+    timestamp = []
+
+    for i in range(len(data['assetPriceHistories'])):
+        price.append(int(data['assetPriceHistories'][i]['price']) / 1e18)
+        timestamp.append((int(data['assetPriceHistories'][i]['timestamp'])))
+
+    df2 = pd.DataFrame(price, index=timestamp, columns=['USD-ETH'])
+
+    df = pd.concat([df1, df2], axis=1, sort=False)
+
+    df['ROI-USD'] = ((df['ROI-ETH'] / df['USD-ETH']) / (1 / df.iloc[0]['USD-ETH']) - 1) * 100
+    df['ROI-ETH'] = (df['ROI-ETH'] - 1) * 100
+    df.index = pd.to_datetime(df.index, unit='s').strftime('%Y-%m-%d')
+    df = df[~df.index.duplicated(keep='first')]
+    df.index.name = 'Date'
+
+    df.to_csv(file_name[:-4] + '_raw.csv', index=True)
+        
     cmc = yf.Ticker("^CMC200")
 
     hist = cmc.history(period="max")[['Open']]
     hist['Open'] = (np.where(hist['Open'] < 100, hist['Open'] * 10, hist['Open']))
+    hist.index = pd.to_datetime(hist.index).strftime('%Y-%m-%d')
 
-    df = pd.concat([df, hist], axis=1, sort=False)
+    df = pd.concat([df, hist], axis=1, sort=True)
     df = df.interpolate(axis=0).dropna(subset=['ROI-USD'])
+
+    df.index = pd.to_datetime(df.index).strftime('%Y-%m-%d')
     
     start_cmc200 = df.head(1)['Open'][0]
     df['CMC200-USD'] = (((df['Open'] - start_cmc200) / start_cmc200)*100)
@@ -124,15 +129,15 @@ def get_fund_GAV_ROI(address, file_name):
 
     data = cg.get_coin_market_chart_by_id('bitcoin', 'eth', 500)
 
-    dates = [datetime.fromtimestamp(data['prices'][i][0] / 1000).strftime('%Y-%m-%d') for i in range(len(data['prices']))]
+    dates = [datetime.datetime.fromtimestamp(data['prices'][i][0] / 1000).strftime('%Y-%m-%d') for i in range(len(data['prices']))]
     price  = [data['prices'][i][1] for i in range(len(data['prices']))]
 
-    df_data = {'BTC-ETH': price, 'Date' : dates} 
-    df_btc = pd.DataFrame(df_data)
-    df_btc = df_btc.set_index('Date')
-    df_btc.index = pd.to_datetime(df_btc.index)
+    df_btc = pd.DataFrame({'BTC-ETH': price}, index=dates)
+    df_btc.index = pd.to_datetime(df_btc.index).strftime('%Y-%m-%d')
+
+    df_btc.drop(df_btc.tail(1).index,inplace=True)
     
-    df = pd.concat([df, df_btc], axis=1, sort=False)
+    df = pd.concat([df, df_btc], axis=1, sort=True)
     df = df.dropna(subset=['ROI-USD'])
     
     start_btc = df.head(1)['BTC-ETH'][0]
